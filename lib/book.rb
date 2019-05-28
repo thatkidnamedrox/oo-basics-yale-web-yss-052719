8d85d24b6a8f68d9b1c4fac9ec364bf6e2e20292
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-28 15:09:39 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,20 +1,9 @@
 class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
   def initialize(title)
     @title = title
   end
-
-  attr_reader :title
-  attr_accessor :title
-
-  attr_writer :author
-  attr_reader :author
-  attr_accessor :author
-
-  attr_writer :page_count
-  attr_accessor :page_count
-
-  attr_writer :genre
-  attr_accessor :genre
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
